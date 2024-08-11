28ae17a888b1433b374f8ffcd667ef28599829eb
jacob-s-unger <jacob.s.unger@gmail.com>
2024-08-11 10:19:36 -0600

    colorscheme is working! + added more packages

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number") -- Makes the files have the line numbers on the side
+vim.cmd("set cc=80") -- Set atn 80 column border for good coding style I guess...
 vim.g.mapleader = " "
 
 -- Bootstrap lazy.nvim 
@@ -35,13 +36,23 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- add your plugins here
-      { "eldritch-theme/eldritch.nvim", lazy = false, priority = 1000, opts = {}},
+      { "eldritch-theme/eldritch.nvim", lazy = false, priority = 1000 , config = true, opts = {}},
+    ---------------------------------------------------------
       { "nvim-telescope/telescope.nvim", tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }}, -- This is the live file system search and grep package
-      { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
+    ---------------------------------------------------------
+      { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    ---------------------------------------------------------
+      { 'nvimdev/dashboard-nvim', event = 'VimEnter', 
+         config = function()
+            require('dashboard').setup {}
+               end,
+         dependencies = { {'nvim-tree/nvim-web-devicons'}}},
+    --------------------------------------------------------
+      { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = { "nvim-lua/plenary.nvim","nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim"}}
+    --------------------------------------------------------
     },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "eldritch" } }, -- This runs the installed color scheme of the plugins loaded
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
@@ -57,4 +68,7 @@ config.setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme eldritch]])
 
