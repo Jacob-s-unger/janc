6a9f09d549e5e40520c8cc6eec85929b9c8777fd
jacob-s-unger <jacob.s.unger@gmail.com>
2024-08-12 12:23:26 -0600

    janc dashboard added - require hotkey fixes

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number") -- Makes the files have the line numbers on the side
-vim.g.mapleader = " "
-
-
-
 -- Bootstrap lazy.nvim 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -25,23 +16,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-
+require("vim_options")
 require("lazy").setup("plugins") --working! :) Plugins are now broken into subfiles in the plugins directory, each subfile is a plug-in that I like
 
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- automatically check for plugin updates
 
--- vim.keymap.set('n', '<C-h>', ':Neotree toggle<CR>', {})
 
 
 
---
+++ lua/vim_options.lua
@@ -0,0 +1,13 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number") -- Makes the files have the line numbers on the side
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+
+
+
+
